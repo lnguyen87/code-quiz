b6b074c38986628e7b6b40bcda2dfc3fd4f14f55
lnguyen87 <luc_nguyen@outlook.com>
2021-11-13 12:47:45 -0700

    added timer to start when startQuiz is clicked

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,18 @@ var startButton = document.querySelector("#start");
 // 
 function startQuiz() {
     console.log("click");
+    var counter = 60;
+    setInterval(function() {
+        counter--;
+        if (counter >= 0) {
+            span = document.getElementById("timer");
+            span.innerHTML = counter;
+        }
+        if (counter === 0) {
+            alert("Sorry, time's up!");
+            clearInterval(counter);
+        }
+    }, 1000);
 }
 
 
