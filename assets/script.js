019433ec14903e96a73965986d30c1a36ca7eae5
lnguyen87 <luc_nguyen@outlook.com>
2021-11-13 17:27:21 -0700

    removed start quiz and high scores button one quiz starts

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,17 @@
 var startButton = document.querySelector("#start");
+var scoreButton = document.querySelector("#highscore");
 
-
-// 
+// Timer runs when start quiz button clicked
 function startQuiz() {
     console.log("click");
     var counter = 60;
+    startButton.remove();
+    scoreButton.remove();
     setInterval(function() {
         counter--;
         if (counter >= 0) {
             span = document.getElementById("timer");
-            span.innerHTML = counter;
+            span.innerHTML = "Time remaining: " + counter;
         }
         if (counter === 0) {
             alert("Sorry, time's up!");
@@ -18,6 +20,8 @@ function startQuiz() {
     }, 1000);
 }
 
-
 // Add event listener to start button
-startButton.addEventListener("click", startQuiz)
+startButton.addEventListener("click", startQuiz);
+
+// Add event lisener to high score button
+scoreButton.addEventListener("click", highScore);
