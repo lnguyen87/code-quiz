88c856ab4374b327cc66379c799490ac64c9db00
lnguyen87 <luc_nguyen@outlook.com>
2021-11-18 09:22:09 -0700

    added highscore and localstorage

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,10 @@ var currentIndex = 0;
 var question = document.querySelector("#question");
 var answers = document.querySelector(".answers");
 
+var scoreContent = document.querySelector("#highscore-content");
+var scoreTitle = document.querySelector("#highscore-title");
+var playerScore = 0;
+
 
 
 //  question and answers array
@@ -25,6 +29,16 @@ var possibleQuestion = [{
     questions: "What is the sum of 2 + 2?",
     answer: ["22", "4", "0", "2"],
     correct: "4",
+},
+{
+    questions: "How many feet are in a yard?",
+    answer: ["3", "6", "9", "12"],
+    correct: "3",
+},
+{
+    questions: "What color is grass?",
+    answer: ["Green", "Orange", "Yellow", "Brown"],
+    correct: "Green",
 }
 ];
 
@@ -61,15 +75,20 @@ function showQuestion() {
         questionContent.style.display = "none";
         clearInterval(counter);
         timer.remove();
+        scoreContent.classList.remove("hide");
+        endQuiz();
         return
     }
     var currentQuestion = possibleQuestion[currentIndex];
     question.textContent = currentQuestion.questions;
     for (var i = 0; i < currentQuestion.answer.length; i++) {
         var answerEl = currentQuestion.answer[i];
-        console.log(answerEl);
         var answerBtn = document.createElement("button");
         answerBtn.textContent = answerEl;
+        answerBtn.style.backgroundColor = "#005780";
+        answerBtn.style.color = "#FFFFFF";
+        answerBtn.style.margin = "10px";
+        answerBtn.style.boxShadow = "1px 1px 2px #000000;"
         answerBtn.setAttribute("value", answerEl);
         answerBtn.onclick = checkAnswer;
         answers.appendChild(answerBtn);
@@ -79,17 +98,57 @@ function showQuestion() {
 // check if answer is correct or incorrect
 function checkAnswer() {
     if (this.value === possibleQuestion[currentIndex].correct) {
-        console.log("Correct");
         console.log(this.value);
+        console.log("Correct");
+        playerScore = playerScore + 10;
         currentIndex++;
         showQuestion();
     } else {
+        console.log(this.value);
         console.log("Incorrect");
+        counter = counter - 5;
+        currentIndex++;
+        showQuestion();
     }
 }
 showQuestion();
+};
+
+var endQuiz = function() {
+    alert("You have completed the quiz. Let's see how you did!");
+
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }    
+
+    if (playerScore >= highScore) {
+        localStorage.setItem("highscore", playerScore);
+        localStorage.setItem("name", playerName);
+
+        alert("You have the high score of " + playerScore + "!");
+    } else {
+        alert("You did not beat the high score of " + highScore + ". Better luck next time!");
+    }
+
+    if (playerScore > 0) {
+        alert("Your score is " + playerScore + " out of 50.");
+    } else {
+        alert("You did not answer any questions correctly.");
+    }
 
 };
+
+var playerName = function() {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("Please enter your name.")
+    }
+    console.log("Player name is " + name);
+    return name;
+};
+
 
 // Add event listener to start button
 startButton.addEventListener("click", startQuiz);
